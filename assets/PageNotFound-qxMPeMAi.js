aea7290d16167caa8944bd38a757b27a6d7b9ed0
Pilot-64 <Pilot-64@users.noreply.github.com>
2024-02-01 20:00:23 +0000

    deploy: f432c0476b0e35fb118bd9cca9d10e8664698bff

---
+++ assets/PageNotFound-qxMPeMAi.js
@@ -0,0 +1,1 @@
+import{j as r}from"./jsx-runtime-JPUeH_4K.js";const t=()=>r.jsx("div",{children:r.jsx("h1",{children:"Error"})});export{t as default};
---
+++ assets/contact-p2JfcHIZ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./jsx-runtime-JPUeH_4K.js";const r=()=>t.jsx("div",{});export{r as default};
